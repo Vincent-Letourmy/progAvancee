242f810178074bda84c4b946fb9d652f19524004
Vincent LETOURMY <vincent.letourmy@etu.umontpellier.fr>
2019-02-12 15:58:16 +0100

    Facteurs premiers

--- ExercicesPlusDifficiles/Exo2/tri/TriTab.java
+++ ExercicesPlusDifficiles/Exo2/tri/TriTab.java
@@ -2,16 +2,9 @@ package Exo2.tri;
 
 public class TriTab {
 
-    /*static int drapeauBicolorAux(int[] t, int i) {
+    static int drapeauBicolorAux(int[] t, int i) {
 
 
-
-
-        for (int j=i;j<t.length;j++){
-            if (t[j]==1){
-                return j;
-            }
-        }
     }
-    }*/
 }
+
