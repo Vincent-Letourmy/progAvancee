41f3482f128616d3588522b08120e52138f6c166
Vincent LETOURMY <vincent.letourmy@etu.umontpellier.fr>
2019-02-26 15:15:35 +0100

    Hanoi debut

--- JeuxRec/Hanoi.java
+++ JeuxRec/Hanoi.java
@@ -1,22 +1,34 @@
 public class Hanoi {
 
-    public static void resoudreAux(int n, int i, int j){
-        if(n==1){
-            System.out.println(i+" -> "+j);
+    public static int resoudreAux(int n, int i, int j) {
+        if (n > 0) {
+            if (n == 1) {
+                System.out.println(i + " -> " + j);
+                return 1;
+            } else {
+                int res = 0;
+                int k = 6 - i - j;
+                res += resoudreAux(n - 1, i, k);
+                res += resoudreAux(1, i, j);
+                res += resoudreAux(n - 1, k, j);
+                return res;
+            }
         }
         else{
-            int k=6-i-j;
-            resoudreAux(n-1,i,k);
-            resoudreAux(1,i,j);
-            resoudreAux(n-1,k,j);
+            return 0;
         }
     }
 
-    public static void resoudre(int n){
-        resoudreAux(n,1,3);
+    public static int resoudre(int n){
+        return resoudreAux(n,1,3);
+    }
+
+    public static double getUn(int n){
+        return Math.pow(2,n)-1;
     }
 
     public static void main(String[] args) {
-        resoudre(3);
+        resoudre(0);
+        System.out.println(getUn(3));
     }
 }
