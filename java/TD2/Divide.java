3a0e708468c6f1f5082a533628a1b5e203894526
Vincent LETOURMY <vincent.letourmy@etu.umontpellier.fr>
2019-03-19 15:52:09 +0100

    quickSort à finir

--- java/TD2/Divide.java
+++ java/TD2/Divide.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class Divide {
 
     static int puiss(int x,int n){
@@ -22,9 +24,41 @@ public class Divide {
         }
     }
 
+    static int pivot (int[]t, int i, int j){
+
+        int[]tRes = new int[t.length];
+        int p = t[j];
+        int c1 = 0;
+        int c2 = j;
+        for (int k = i; k <= j; k++) {
+            if (t[k]>p){
+                tRes[c2] = t[k];
+                c2--;
+            }
+            else{
+                tRes[c1] = t[k];
+                c1++;
+            }
+        }
+        for (int k = i; k <= j; k++) {
+            t[k]=tRes[k];
+        }
+        return c1;
+    }
+
+    static void quickSort(int[]t, int i,int j){
+
+
+
+    }
+
     public static void main(String[] args) {
 
         System.out.println(puiss(2,5));
         System.out.println(puissRapideAux(2,5));
+
+        int[]t = {2,10,4,9,1,6};
+        System.out.println(pivot(t,0,5));
+        System.out.println(Arrays.toString(t));
     }
 }
