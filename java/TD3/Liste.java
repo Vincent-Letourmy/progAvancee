2aec4a24a8e445d6f85c40c0c457024131490e3b
Vincent LETOURMY <vincent.letourmy@etu.umontpellier.fr>
2019-04-02 16:00:40 +0200

    liste début

--- java/TD3/Liste.java
+++ java/TD3/Liste.java
@@ -21,8 +21,69 @@ class Liste{
 
     int longueur(){
         if(this.estVide())
-            return 1;
+            return 0;
         else
             return (suiv.longueur() + 1);
+    }
+
+    boolean recherche(int x){
+        if (this.estVide()){
+            return false;
+        }
+        if (this.val == x){
+            return true;
+        }
+        else{
+            return suiv.recherche(x);
+        }
+    }
+
+    boolean croissant(){
+        if (this.estVide()){
+            return true;
+        }
+        if (suiv.estVide()){
+                return true;
+        }
+        else{
+            if (this.val <= suiv.val){
+                return suiv.croissant();
+            }
+            else{
+                return false;
+            }
+        }
+    }
+
+    public void ajoutTete(int x){
+        Liste aux = new Liste();
+        aux.val = val;
+        aux.suiv = suiv;
+        this.val = x;
+        this.suiv = aux;
+    }
+
+    public void supprimeTete(){
+                                        /** sur liste non vide */
+        this.val = suiv.val;
+        this.suiv = this.suiv.suiv;
+    }
+
+    void ajoutFin(int x){
+        if (this.estVide()){
+            ajoutTete(x);
+        }
+        else suiv.ajoutFin(x);
+    }
+
+    public String toString(){
+        String res ="";
+        if(this.estVide()){
+            return res;
+        }
+        else {
+            res = " " + this.val;
+            return res + suiv.toString();
+        }
     }
 }
--- java/TD3/Main.java
+++ java/TD3/Main.java
@@ -4,15 +4,29 @@ public class Main {
         Liste l1 = new Liste();
         Liste l2 = new Liste();
         Liste l3 = new Liste();
+        Liste Def = new Liste();
 
-        l1.setVal(1);
-        l2.setVal(2);
-        l3.setVal(3);
+        l1.setVal(2);
+        l2.setVal(5);
+        l3.setVal(9);
 
         l1.setSuiv(l2);
         l2.setSuiv(l3);
+        l3.setSuiv(Def);
 
-        System.out.println(l1.longueur());
+        Def.ajoutTete(6);
+        Def.ajoutTete(5);
+        Def.ajoutTete(7);
+        Def.ajoutFin(9);
+        Def.ajoutTete(4);
+
+        System.out.println(Def.longueur());
+        System.out.println(Def);
+
+        //System.out.println(l1.longueur());
+        //System.out.println(l1.recherche(3));
+        //System.out.println(Def.croissant());
+
 
     }
 }
