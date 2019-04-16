bc85247cfca9bfeec958e8a93f94711af61cc1b1
Vincent LETOURMY <vincent.letourmy@etu.umontpellier.fr>
2019-04-16 14:14:09 +0200

    debut polynome

---
+++ java/Polynome/Polynome.java
@@ -0,0 +1,26 @@
+package Polynome;
+
+class Polynome{
+    private int coeff;
+    private int deg; //deg >= 0
+    private Polynome suiv;
+    //represente le monosme coeff∗x^{deg}
+//suiv==null represente le polynome nul
+    public Polynome(){
+        suiv = null;
+    }
+    public boolean estVide(){
+        return suiv==null;
+    }
+    public Polynome(Polynome p){
+//constructeur par copie en profondeur
+        if(p.estVide()){
+            suiv=null;
+        }
+        else{
+            coeff = p.coeff;
+            deg = p.deg;
+            suiv = new Polynome(p.suiv);
+        }
+    }
+}
